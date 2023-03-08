6c563b54a4d9dd81744be81cfd657de8c1e3935f
Marc <a17507@iesberenguer.net>
2023-03-08 13:38:15 +0100

    modificat

--- Berenguify.py
+++ Berenguify.py
@@ -18,7 +18,7 @@ while True:
     try:
         opcio=int(input("introdueix una opcio de l'1 al 5: "))
         print() 
-        
+
         if opcio==1:
             #Marc: Responsable de la validació d'alta cançons
             print("                         ♬ :;;;:♬  Canco 1 ♬ :;;;:♬")
@@ -41,33 +41,8 @@ while True:
                 canco_nova[4] = input("Introdueix l'enllac2: ")
                 comprovacio = re.search(expresio, canco_nova[4])
             print("es correcte")
-            cancons.append(list(canco_nova))               
-
-            print("                         ♬ :;;;:♬  Canco 2 ♬ :;;;:♬"                                )
-            canco_nova[0]=input("*★ *――――*★ *Introdueix el nombre de la canco*★ *――――*★ *: ")
-            canco_nova[1]=input("*★ *――――*★ *Introdueix l'autor de la canco*★ *――――*★ *: ")
-            canco_nova[2]=input("*★ *――――*★ *Introduiex l'any de llancament de la canco*★ *――――*★ *: ")
-            expresio = "^[0-9]{4}$"
-            comprovacio = re.search(expresio, canco_nova[2])
-            while not comprovacio:
-                print("es incorrecte")
-                canco_nova[2]=input("*★ *――――*★ *Introduiex l'any de llancament de la canco*★ *――――*★ *: ")
-                comprovacio = re.search(expresio, canco_nova[2])
-            print("es correcte")
-            canco_nova[3]=input("*★ *――――*★ *Introdueix l'estil de la canco*★ *――――*★ *: ")
-            canco_nova[4] = input("Introdueix l'enllac1: ")
-            expresio = "^https://"
-            comprovacio = re.search(expresio, canco_nova[4])
-            while not comprovacio:
-                print("es incorrecte")
-                canco_nova[4] = input("Introdueix l'enllac2: ")
-                comprovacio = re.search(expresio, canco_nova[4])
-            print("es correcte")
-            cancons.append(list(canco_nova))
-                
+            cancons.append(list(canco_nova))              
             
-            
-    
         elif opcio==2:
             print("                                ✦ Usuari 1✦")
             nomcomplet=input("⇉ Escriu el teu nom i llinatge: ")
@@ -104,40 +79,7 @@ while True:
                 
                 
             poblacio=input("⇉ Escriu la teva població: ")
-            print("                                ✦ Usuari 2✦")
-            nomcomplet1=input("⇉ Escriu el teu nom i llinatge: ")
-            datanaixement1=input("⇉ Escriu la data de naixement: ") 
-            comprobacio = re.search(expresiodata, datanaixement1)
-            if comprobacio:
-                print("es correcte")
-            else:
-                print("es incorrecte")
-                datanaixement1=input("⇉ Escriu la teva data de naixement: ")
-                comprobacio = re.search(expresiodata, datanaixement1)
-                if comprobacio:
-                    print("Es correcte")
-                else:
-                    print("Ja has fet els 2 intents")
-                    sys.exit()
-                
-            
-            
-            correu1=input("⇉ Escriu el teu correu: ")
-            comprobacio = re.search(expresiocorreu, correu1)
-            if comprobacio:
-                print("es correcte")
-            else:
-                print("es incorrecte")
-                correu1=input("⇉ Escriu el teu correu: ")
-                comprobacio = re.search(expresiocorreu, correu1)
-                if comprobacio:
-                    print("Es correcte")
-                else:
-                    print("Ja has fet els 2 intents")
-                    sys.exit()
-            poblacio1=input("⇉ Escriu la teva població: ")
-        
-        
+           
         elif opcio==3:
             
             canco_1="La Ramona"
